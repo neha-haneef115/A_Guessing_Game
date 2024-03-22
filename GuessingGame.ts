07371f22efa7ad8ba90eec9e6ce6ad2f7347eeb0
neha-haneef115 <160058788+neha-haneef115@users.noreply.github.com>
2024-03-23 02:16:26 +0500

    Add files via upload
    A simple yet an exhilarating Guessing Game.

---
+++ GuessingGame.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("Number Guessing Game");
+console.log("=======================");
+const answer=await inquirer.prompt([{message:"Enter your number: ", type:"number",name:"GuessNumber"}])
+let guessingnumber=Math.floor(Math.random()*6+1)
+
+if(answer.GuessNumber===guessingnumber){
+    console.log("Congratulation! Your guess is right.");
+    }
+    else
+    {
+        console.log("Oops you guess was wrong.");
+        
+    }
+    
